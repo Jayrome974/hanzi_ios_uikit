e85a0db16e84fd0094bfe4244761d1cc6b97a3ec
Jay <jayrome82@gmail.com>
2020-11-08 18:42:19 -0500

    Init project structure

--- Learn Useful Words/ViewController.swift
+++ Learn Useful Words/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordsTableView: UITableView!
+    
+    var words: [Word] = [
+        Word(value: "家", nbOccurrences: 5),
+        Word(value: "累", nbOccurrences: 3)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Learn Useful Words/Word.swift
@@ -0,0 +1,13 @@
+//
+//  Word.swift
+//  Learn Useful Words
+//
+//  Created by Jay on 2020-11-08.
+//
+
+import Foundation
+
+struct Word {
+    let value: String
+    let nbOccurrences: Int
+}
