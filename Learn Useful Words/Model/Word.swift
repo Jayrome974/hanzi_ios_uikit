c324e18bf828223150f0301585f9925cb89c03cd
Jay <jayrome82@gmail.com>
2020-11-12 23:06:30 -0500

    feat init display words list

--- Learn Useful Words/Model/Word.swift
+++ Learn Useful Words/Model/Word.swift
@@ -2,7 +2,7 @@
 //  Word.swift
 //  Learn Useful Words
 //
-//  Created by Jay on 2020-11-08.
+//  Created by Jay on 2020-11-12.
 //
 
 import Foundation
--- Learn Useful Words/ViewController.swift
+++ Learn Useful Words/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // TODO move to constants struct
+    let cellIdentifier = "ReusableCell"
+    
     @IBOutlet weak var wordsTableView: UITableView!
     
     var words: [Word] = [
@@ -19,8 +22,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        wordsTableView.dataSource = self
+        navigationItem.title = "Library"
+        
+//        navigationController?.navigationBar.prefersLargeTitles = true
+//        let appearance = UINavigationBarAppearance()
+//        appearance.backgroundColor = .black
+////        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+//        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+////        navigationController?.navigationBar.tintColor = .white
+//        navigationController?.navigationBar.standardAppearance = appearance
+//        navigationController?.navigationBar.compactAppearance = appearance
+//        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        var cellContentConfiguration = cell.defaultContentConfiguration();
+        cellContentConfiguration.text = words[indexPath.row].value
+        cell.contentConfiguration = cellContentConfiguration
+        return cell
+    }
+}
